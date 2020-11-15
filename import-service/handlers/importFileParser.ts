50ffc32dcf4fff9e7b0ae1accd3846694cdb38d5
SKolevatov <sergei.kolevatov@nytimes.com>
2020-11-15 09:03:20 -0500

    feat: tried with async/await

--- import-service/handlers/importFileParser.ts
+++ import-service/handlers/importFileParser.ts
@@ -2,47 +2,57 @@ import { S3Handler } from 'aws-lambda';
 import { S3 } from 'aws-sdk';
 import csvParser from 'csv-parser';
 import 'source-map-support/register';
+import 'stream';
+import * as stream from "stream";
 
 const BUCKET = 'rss-task5';
 
 
-export const importFileParser: S3Handler = (event) => {
+export const importFileParser: S3Handler = async (event) => {
     const s3 = new S3({region: 'eu-west-1'});
     console.log('event:', event);
     const records = event.Records;
-    records.map((record) => {
+    records.map(async (record) => {
         console.log(record);
         const key = record.s3.object.key;
         console.log(key);
-        s3.getObject({
+
+        const s3object = (await s3.getObject({
             Bucket: BUCKET,
             Key: key
-        }).createReadStream()
-            .on('error', err => console.error(err))
+        }).promise());
+        const csvReadStream = new stream.Readable();
+        csvReadStream._read = () => {};
+        csvReadStream.push(s3object.Body);
+
+        csvReadStream
+            .on('error', err => {
+                console.log(err);
+            })
             .pipe(csvParser())
             .on('data', data => console.log(`DATA from ${key}:`, data))
             .on('end', () => {
                 console.log(`${key} is read`);
-                s3.copyObject({
-                    Bucket: BUCKET,
-                    CopySource: `${BUCKET}/${key}`,
-                    Key: key.replace('uploaded', 'parsed')
-                }, (err) => {
-                    if (err) {
-                        console.warn(`failed to copy ${key}`, err);
-                    }
-                    console.log(`copied ${key}, deleting...`);
-                    s3.deleteObject({
-                        Bucket: BUCKET,
-                        Key: key
-                    }, (err) => {
-                        if (err) {
-                            console.warn(`failed to delete file ${key}`, err)
-                        }
-                    });
-                });
             })
-            .on('error', err => console.error(err));
+            .on('error', err => {
+            console.error(err);
+        });
 
+        console.log(`Copying ${key}...`);
+        await s3.copyObject({
+            Bucket: BUCKET,
+            CopySource: `${BUCKET}/${key}`,
+            Key: key.replace('uploaded', 'parsed')
+        }).promise().catch((err) => {
+            console.warn(`failed to copy ${key}`, err);
+        });
+        console.log(`Copied. Deleting ${key}...`);
+        await s3.deleteObject({
+                Bucket: BUCKET,
+                Key: key
+            }).promise().catch((err) => {
+                    console.warn(`failed to delete file ${key}`, err)
+            });
+        console.log(`Deleted ${key}`);
     })
 };
--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -25,6 +25,11 @@ const serverlessConfiguration: Serverless = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      PG_HOST: 'task4-instance.c4bpr1tbr0zy.eu-west-1.rds.amazonaws.com',
+      PG_PORT: 5432,
+      PG_DATABASE: 'postgres',
+      PG_USERNAME: 'postgres',
+      PG_PASSWORD: 'GmXTsYxuhykU5Somi9Dn'
     },
   },
 
